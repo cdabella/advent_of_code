32c19096b4099b119b7dab369b957c0a75e7cf43
Chris Abella <abella.cd@gmail.com>
2022-12-07 23:42:14 -0600

    2022 Day 7

--- 2022/07/07.py
+++ 2022/07/07.py
@@ -12,58 +12,64 @@ dir_file = "dir"
 
 
 class Directory:
-    def __init__(self, name, parent, files=[], dirs=[]):
+    def __init__(self, name, parent, files=[], child_dirs={}):
         self.name = name
         self.parent = parent
         self.files = files
-        self.dirs = dirs
-        self.size = 0
+        self.child_dirs = child_dirs
+        self.file_size = 0
+        self.total_size = 0
 
     def __repr__(self):
         return str({"name": self.name, "parent": self.parent.name})
 
 
-def parse_dir(dir: Directory, filter_size=100000):
+def filter_dir_by_size(dir: Directory, filter_size=100000):
     subdirs_total_size = 0
     matching_dirs = []
-    for child_dir in dir.dirs:
-        subdir_size, matching_subdirs = parse_dir(child_dir)
-
+    for child_dir in dir.child_dirs.values():
+        subdir_size, matching_subdirs = filter_dir_by_size(child_dir, filter_size)
+        matching_dirs.extend(matching_subdirs)
         subdirs_total_size += subdir_size
-    total_size = subdirs_total_size + dir.size
+    total_size = subdirs_total_size + dir.file_size
+    dir.total_size = total_size
+    if total_size <= filter_size:
+        matching_dirs.append((dir.name, total_size))
+    return total_size, matching_dirs
 
 
-lines = """$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k""".split(
-    "\n"
-)
+# lines = """$ cd /
+# $ ls
+# dir a
+# 14848514 b.txt
+# 8504156 c.dat
+# dir d
+# $ cd a
+# $ ls
+# dir e
+# 29116 f
+# 2557 g
+# 62596 h.lst
+# $ cd e
+# $ ls
+# 584 i
+# $ cd ..
+# $ cd ..
+# $ cd d
+# $ ls
+# 4060174 j
+# 8033020 d.log
+# 5626152 d.ext
+# 7214296 k""".split(
+#     "\n"
+# )
 
 
 def pt1():
-    dirs = {}
-    current_dir = Directory(name="/", parent="/")
-    idx = 0
+    root_dir = Directory(name="/", parent="/")
+    root_dir.parent = root_dir
+    current_dir = root_dir
+    idx = 1  # skip "cd /"
     while idx < len(lines):
         input = lines[idx]
         cmd = input[:4]
@@ -72,40 +78,57 @@ def pt1():
             if args == cd_up:
                 current_dir = current_dir.parent
             else:
-                current_dir = dirs.get(args, Directory(name=args, parent=current_dir))
-                dirs[args] = current_dir
+                current_dir = current_dir.child_dirs[args]
             idx += 1
         elif cmd == ls:
             files = []
-            child_dirs = []
+            child_dirs = {}
             idx += 1
             size = 0
             while idx < len(lines) and lines[idx][0] != cmd_start:
                 if lines[idx][:3] == dir_file:
                     child_dir_name = lines[idx].split()[1]
-                    child_dir = dirs.get(
-                        child_dir_name,
-                        Directory(name=child_dir_name, parent=current_dir),
-                    )
-                    child_dirs.append(child_dir)
-                if lines[idx][:3] != dir_file:
+                    child_dir = Directory(name=child_dir_name, parent=current_dir)
+                    child_dirs[child_dir_name] = child_dir
+                elif lines[idx][:3] != dir_file:
+                    # if current_dir.name == "gdqqtvnp":
+                    #     breakpoint()
                     file_size, file_name = tuple(lines[idx].split())
                     file_size = int(file_size)
                     files.append((file_size, file_name))
                     size += file_size
                 idx += 1
-            print(current_dir.name, files, child_dirs)
             current_dir.files = files
-            current_dir.dirs = child_dirs
-            current_dir.size = size
-    print(dirs)
-    print(parse_dir(dirs["/"]))
+            current_dir.child_dirs = child_dirs
+            current_dir.file_size = size
+
+    _, matching_dirs = filter_dir_by_size(root_dir)
+    total = sum([dir[1] for dir in matching_dirs])
+    print(total)
+    # submit(total)
+    return root_dir
 
 
-def pt2():
-    pass
+def list_all_dir_sizes(dir: Directory):
+    sizes = [dir.total_size]
+    for child_dir in dir.child_dirs.values():
+        child_dir_sizes = list_all_dir_sizes(child_dir)
+        sizes.extend(child_dir_sizes)
+    return sizes
+
+
+def pt2(root_dir):
+    total_space = 70000000
+    unused_space_target = 30000000
+    unused_space = total_space - root_dir.total_size
+    delete_size = unused_space_target - unused_space
+    answer = sorted(
+        [size for size in list_all_dir_sizes(root_dir) if size > delete_size]
+    )[0]
+    print(answer)
+    # submit(answer)
 
 
 if __name__ == "__main__":
-    pt1()
-    # pt2()
+    root_dir = pt1()
+    pt2(root_dir)
